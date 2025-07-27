f3bde47056ad813398625e4105c20d991c47ed5f
Jordan Scales <scalesjordan@gmail.com>
2025-07-27 14:31:37 -0400

    put everything in self.included

--- html.rb
+++ html.rb
@@ -1,20 +1,22 @@
 module HtmlBuilder
-  class << self
-    private
-    def define_tag(base, name)
-      base.define_method(name) do |**attrs, &block|
+  def self.included(base)
+    tags = [
+      # top level tags
+      :html, :head, :body,
+
+      # head tags
+      :title, :meta,
+    ]
+
+    tags.each do |tag|
+      base.define_method(tag) do |**attrs, &block|
         attr_string = attrs.map { |k, v| %{#{k}="#{v}"} }.join " "
         attr_string = " #{attr_string}" unless attr_string.empty?
 
         children_html = block ? block.() : ""
 
-        "<#{name}#{attr_string}>#{children_html}</#{name}>"
+        "<#{tag}#{attr_string}>#{children_html}</#{tag}>"
       end
     end
-  end
-
-  def self.included(base)
-    define_tag(base, :html)
-    define_tag(base, :body)
   end
 end
