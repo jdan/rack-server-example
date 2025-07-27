db005a15092f3d50a21b2f995052b0f4db303787
Jordan Scales <scalesjordan@gmail.com>
2025-07-27 14:24:50 -0400

    use a private module method

--- html.rb
+++ html.rb
@@ -1,6 +1,7 @@
 module HtmlBuilder
-  def self.included(base)
-    tag = ->(name) do
+  class << self
+    private
+    def define_tag(base, name)
       base.define_method(name) do |**attrs, &block|
         attr_string = attrs.map { |k, v| %{#{k}="#{v}"} }.join " "
         attr_string = " #{attr_string}" unless attr_string.empty?
@@ -10,8 +11,10 @@ module HtmlBuilder
         "<#{name}#{attr_string}>#{children_html}</#{name}>"
       end
     end
+  end
 
-    tag.(:html)
-    tag.(:body)
+  def self.included(base)
+    define_tag(base, :html)
+    define_tag(base, :body)
   end
 end
