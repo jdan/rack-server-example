d54580276728a173497c9b818d2a1fd29e168eb6
Jordan Scales <scalesjordan@gmail.com>
2025-07-27 12:50:21 -0400

    /advice

--- hello_world.rb
+++ hello_world.rb
@@ -1,5 +1,19 @@
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    [200, {'content-type' => 'text/plain'}, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when '/'
+      [200, {'content-type' => 'text/plain'}, ['Hello World!']]
+    when '/advice'
+      piece_of_advice = Advice.new.generate
+      [200, {'content-type' => 'text/plain'}, [piece_of_advice]]
+    else
+      [
+        404,
+        {'content-type' => 'text/plain', "content-length" => "13"},
+        ["404 Not Found"]
+      ]
+    end
   end
 end
