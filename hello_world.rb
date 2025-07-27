a21d4ece3268b0eb471036647e005e5376d3a939
Jordan Scales <scalesjordan@gmail.com>
2025-07-27 15:20:15 -0400

    use HtmlBuilder

--- hello_world.rb
+++ hello_world.rb
@@ -1,26 +1,49 @@
 require_relative 'advice'
+require_relative 'html'
 
 class HelloWorld
+  include HtmlBuilder
+
   def call(env)
     case env['REQUEST_PATH']
     when '/'
       [
         200,
         {'content-type' => 'text/html'},
-        ["<html><body><h2>Hello World!</h2></body></html>"]
+        [
+          html {
+            body {
+              h2 { "Hello World!"}
+            }
+          }
+        ]
       ]
     when '/advice'
       piece_of_advice = Advice.new.generate
       [
         200,
         {'content-type' => 'text/html'},
-        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+        [
+          html {
+            body {
+              b {
+                em { piece_of_advice }
+              }
+            }
+          }
+        ]
       ]
     else
       [
         404,
         {'content-type' => 'text/html', "content-length" => "48"},
-        ["<html><body><h4>404 Not Found</h4></body></html>"]
+        [
+          html {
+            body {
+              h4 { "404 Not Found" }
+            }
+          }
+        ]
       ]
     end
   end
